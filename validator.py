97e76ee44c685de83216959bc2d3dd634123341b
isaiahiyede <isaiahiyede.ca@gmail.com>
2023-02-21 15:46:38 +0100

    added validation rules

---
+++ validator.py
@@ -0,0 +1,23 @@
+import json
+import re
+
+# validation for individual borrower
+def validation_1(data_file):
+    # check validation rules
+    return True
+
+# validation for Credit Information
+def validation_2(data_file):
+    return True
+
+# validation for Corporate Borrower Information
+def validation_3(data_file):
+    return True
+
+# validation for Guarantor Information
+def validation_4(data_file):
+    return True
+
+# validation for Principal Template
+def validation_5(data_file):
+    return True
