e3fdbd92669b6aef70b21392c0ea593f42f44add
isaiahiyede <isaiahiyede.ca@gmail.com>
2023-02-23 15:51:17 +0100

    cleared all logs files

--- review.py
+++ review.py
@@ -24,14 +24,20 @@ def checkSheetsInFile(file)->any:
     number_of_sheets_in_file = ""
     file_extention = file.split(".")
     if file_extention[1] in ["xlsx","xls","csv"]:
+        if file_extention[1] == "csv":
+            read_file = pd.read_csv(getBaseDirectory()+"/"+file)
+            read_file.to_excel(getBaseDirectory()+"/"+file_extention[0]+".xlsx", index = None, header=True)
+            file = file_extention[0]+".xlsx"
+        print(file)
         file_obj = pd.ExcelFile(getBaseDirectory()+"/"+file)
         number_of_sheets_in_file = len(file_obj.sheet_names)
     else:
         pass
-    return number_of_sheets_in_file, file_obj.sheet_names
+    print(number_of_sheets_in_file, file_obj.sheet_names)
+    return number_of_sheets_in_file, file_obj.sheet_names, file
 
 # transform columns
-def transformColumns(list_of_values):
+def transformColumns(list_of_values)->any:
     pattern = "[ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]"
     new_list = [re.sub(pattern, "", x.upper()) for x in list_of_values]
     return new_list
@@ -39,11 +45,14 @@ def transformColumns(list_of_values):
 # check the file type
 def checkFileType(data_file_columns)->any:
     items_list = transformColumns(data_file_columns)
+    print(items_list)
     if all(items in items_list for items in ["BVNNO","SURNAME","FIRSTNAME","MIDDLENAME"]):
         return "Individual Borrower"
-    elif all(items in items_list for items in ["ACCOUNTNUMBER","ACCOUNTSTATUS","ACCOUNTSTATUSDATE","OVERDUEAMOUNT","OUTSTADINGBALANCE"]):
+    elif all(items in items_list for items in ["ACCOUNTNUMBER","ACCOUNTSTATUS","ACCOUNTSTATUSDATE","OVERDUEAMOUNT","OUTSTANDINGBALANCE"]):
         return "Credit Information"
-    elif all(items in items_list for items in ["BUSINESIDENTIFICATIONNUMBER","BUSINESSCORPORATENAME","BUSINESSCORPORATETYPE","BUSINESSCATEGORY"]):
+    elif all(items in items_list for items in ["DATEOFINCORPORATION","BUSINESIDENTIFICATIONNUMBER","BUSINESSCORPORATETYPE","BUSINESSCATEGORY"]):
+        return "Corporate Borrower Information"
+    elif all(items in items_list for items in ["DATEOFINCORPORATION","BUSINESSNAME","BUSINESSIDENTIFICATIONNO","DATEOFINCORPORATION"]):
         return "Corporate Borrower Information"
     elif all(items in items_list for items in ["GUARANTORSBVN","GUARANTORSPRIMARYADDRESSLINE1","GUARANTORSDATEOFBIRTHINCORPORATION","GUARANTEESTATUSOFLOAN"]):
         return "Guarantor Information"
@@ -53,27 +62,33 @@ def checkFileType(data_file_columns)->any:
         return False
 
 # validate the coulms based on file type
-def validateColumns(data_file_columns_length, file_type):
+def validateColumns(data_file_columns_length, file_type)->any:
     dict_obj_for_file_types = {"Individual Borrower": 46,"Credit Information": 29,"Corporate Borrower Information": 20,
                                "Guarantor Information": 22,"Principal Template": 41}
     if data_file_columns_length ==  dict_obj_for_file_types[file_type]:
         return True
     return False
 
-# validate and review file based on rules
-def review_file(data_file,file_type)->any:
-    dict_of_validation_rules = {"Individual Borrower":validator.validation_1(data_file),
-                                "Credit Information": validator.validation_2(data_file), 
-                               "Corporate Borrower Information":validator.validation_3(data_file),
-                               "Guarantor Information":validator.validation_4(data_file),
-                               "Principal Template": validator.validation_5(data_file)}
-    validation_result = dict_of_validation_rules[file_type]    
+# # validate and review file based on rules
+def review_file(file_type,data_file)->any:
+    dict_of_validation_rules = {"Individual Borrower":validator.individualborrower(file_type, data_file),
+                                "Credit Information": validator.creditinformation(file_type, data_file), 
+                               "Corporate Borrower Information":validator.corporateborrowerinformation(file_type, data_file),
+                               "Guarantor Information":validator.guarantorinformation(file_type, data_file),
+                               "Principal Template": validator.principaltemplate(file_type, data_file)}
+    validation_result = dict_of_validation_rules[file_type] 
     return True
+
+# validate and review file based on rules
+# to be reworked.............
+# def review_file(file_type,data_file)->any:
+#     validator_obj = validator.validator_to_use(file_type,data_file) 
+#     validation_result = validator.validator_obj(file_type,data_file)  
+#     return True
 
 # log to logger file
 def log_to_logger_file(file,sheet,columns,len_data_file,file_type)->any:
-    return iLog(f"file Name: {file}\nSheet Name: {sheet}\nColumns: {columns}\n\
-                    No of Columns: {len_data_file}\nfile type: {file_type}\n\-------------------------------------------------------")
+    return iLog(f"file Name: {file}\nSheet Name: {sheet}\nColumns: {columns}\nNo of Columns: {len_data_file}\nfile type: {file_type}\n-------------------------------------------------------")
 
 # deconstruct files with multiple sheets
 def processFiles(file,sheet_names)->any:
@@ -89,7 +104,8 @@ def processFiles(file,sheet_names)->any:
                 if not isValidColumns:
                     log_to_logger_file(file,sheet,data_file.columns,len(data_file.columns),file_type)
                 else:
-                    review_file(data_file,file_type)
+                    # this is where the majic starts
+                    review_file(file_type,data_file)
                     print(True)
                     print(f"file Name: {file}\nSheet Name: {sheet}\nColumns: {data_file.columns}\n\
                     No of Columns: {len(data_file.columns)}\nfile type: {file_type}\n\
@@ -114,7 +130,7 @@ def readIndividualFilesInDirectory(list_of_files)->any:
         try:
             checkFile = checkSheetsInFile(file)
             if checkFile[0] >= 1:
-                processFiles(os.getcwd()+"/"+"filesToprocess"+"/"+file,checkFile[1])
+                processFiles(os.getcwd()+"/"+"filesToprocess"+"/"+checkFile[2],checkFile[1])
             else:
                 valueToWrite = file,f"Oops no sheet in this file"
                 writeErrorToTextFile(valueToWrite), iLog(valueToWrite)
--- validator.py
+++ validator.py
@@ -1,23 +1,85 @@
 import json
 import re
 
+# validator to use
+# to be reworked.............
+# def validator_to_use(file_type,data_file):
+#     pattern = "[ _]"
+#     file_type_name = re.sub(pattern, "", file_type.lower())
+#     return_obj = f'{file_type_name(file_type,data_file)}'
+#     return eval(return_obj)
+
+# field must return value
+def isTrue():
+    return True
+
 # validation for individual borrower
-def validation_1(data_file):
+def individualborrower(file_type,data_file):
     # check validation rules
+    # 1 rule: specify count of columns
+    # 2 rule: customerID not null and 20 characters in all
+    # 3 rule: set branchcode to '001' is if null
+    # 4 rule: firstname not null
+    # 5 rule: last name not null
+    # 6 rule: middle name optional but if provided more than 2 characters and less than 20 characters in length without space
+    # 7 rule: date of birth not null and format should be 'DD/MM/YYY'
+    # 8 rule: National Identity Number optional but if provided more than 2 characters and less than 20 characters in length without space
+    # 9 rule: Drivers License No optional but if provided more than 2 characters and less than 20 characters in length without space
+    # 10 rule: BVN mandatory and must be 11 chracters in length
+    
+    # 11 rule: Passport No optional but if provided more than 2 characters and less than 20 characters in length without space
+    # 12 rule: Gender not null ("male","female","m","f")
+    # 13 rule: Nationality not null ("Nigeria","NG")
+    # 14 rule: Marital Status not null ("single","married","divorced","widow","widower")
+    # 15 rule: Mobile number not null - must not have special characters
+    # 16 rule: Primary Address Line 1 mandatory but more than 2 characters and less than or equal to 100 characters in length
+    # 17 rule: Primary Address Line 2 not mandatory but if provided must be more than 2 characters and less or equal to 100 characters in length
+    # 18 rule: Primary city/LGA mandatory but more than 2 characters and less than or equal to 100 characters in length
+    # 19 rule: Primary State mandatory but more than 2 characters and less than or equal to 15 characters in length
+    # 20 rule: Primary Country mandatory but more than 2 characters and less than or equal to 15 characters in length
+    
+    # 21 rule: Employment Status mandatory ("employed","unemployed","self employed")
+    # 22 rule: Occupation mandatory but more than 2 characters and less than or equal to 15 characters in length
+    # 23 rule: Business Category mandatory
+    # 24 rule: Business Sector mandatory
+    # 25 rule: Borrower Type mandatory
+    
+    # 26 rule: Other ID not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 27 rule: Tax ID not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 28 rule: Picture File Path not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 29 rule: E-mail address not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 30 rule: Employer Name not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 31 rule: Employer Address Line 1 not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 32 rule: Employer Address Line 2 not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 33 rule: Employer City not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 34 rule: Employer State not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 35 rule: Employer Country not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 36 rule: Title not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 37 rule: Place of Birth not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 38 rule: Work phone not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 39 rule: Home phone not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 40 rule: Secondary Address Line 1 not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 41 rule: Secondary Address Line 1 not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 42 rule: Secondary Address City/LGA not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 43 rule: Secondary Address State not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 44 rule: Secondary Address Country not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 45 rule: Spouse's Surname not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 46 rule: Spouse's First name not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
+    # 47 rule: Spouse's Middle name not mandatory but more than 2 characters and less than or equal to 100 characters in length if provided
     return True
 
 # validation for Credit Information
-def validation_2(data_file):
+def creditinformation(file_type,data_file):
     return True
 
 # validation for Corporate Borrower Information
-def validation_3(data_file):
+def corporateborrowerinformation(file_type,data_file):
     return True
 
 # validation for Guarantor Information
-def validation_4(data_file):
+def guarantorinformation(file_type,data_file):
     return True
 
 # validation for Principal Template
-def validation_5(data_file):
+def principaltemplate(file_type,data_file):
     return True
